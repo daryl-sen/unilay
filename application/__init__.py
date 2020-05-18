fd83de349fd053b4ddf0e8e516c7865588927b2f
Daryl <55649534+daryl-sen@users.noreply.github.com>
2020-05-17 22:25:42 -0700

    added some css

--- application/__init__.py
+++ application/__init__.py
@@ -3,5 +3,7 @@ from application.secrets import secret_key
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = secrets.secret_key
+
 from application.core.views import core
 app.register_blueprint(core, url_prefix="/")
