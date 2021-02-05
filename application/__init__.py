00e08516a191c5d944477cc3c98a3036c4a68504
daryl <tsdaryl@gmail.com>
2021-02-04 18:51:12 -0800

    initial set up

--- application/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import application.secrets as secrets
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = secrets.secret_key
-
-from application.core.views import core
-app.register_blueprint(core, url_prefix="/")
